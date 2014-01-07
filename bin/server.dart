c72005c7d5c616cb16cc4a418b4a0658b71a2f26
joris hermans <JHermans85@hotmail.com>
2014-01-07 12:48:51 +0100

    all files

---
+++ bin/server.dart
@@ -0,0 +1,52 @@
+library multiplayer_example_force;
+
+import 'dart:async';
+import 'dart:io';
+import 'package:logging/logging.dart' show Logger, Level, LogRecord;
+import 'package:force/force_serverside.dart';
+
+final Logger log = new Logger('ChatApp');
+
+void main() {
+  // Set up logger.
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+  
+  var portEnv = Platform.environment['PORT'];
+  var port = portEnv == null ? 8080 : int.parse(portEnv);
+  
+  ForceServer fs = new ForceServer(host: "0.0.0.0", port: port, startPage: "game.html" );
+  
+  // game setting
+  fs.on('play', (e, sendable) {  
+    
+  });
+  
+  // Profile shizzle
+  List<String> playerList = new List<String>();
+  fs.onProfileChanged.listen((e) {
+    String name = e.profileInfo['name'];
+    if (e.type == ForceProfileType.New) {
+      playerList.add(name);
+      
+      fs.send('entered', { 'name' : name });
+    }
+    if (e.type == ForceProfileType.Removed) {
+      playerList.remove(name);
+      
+      fs.send('leaved', { 'name' : name });
+    }
+  });
+  
+  fs.on('list', (e, sendable) { 
+    sendable.sendTo(e.wsId, 'list', playerList);
+  });
+  
+  fs.start().then((_) {
+    fs.serve("/client.dart").listen((request) { 
+      fs.serveFile("../web/client.dart", request);
+    });
+  });
+}
---
+++ web/client.dart
@@ -0,0 +1,127 @@
+import 'dart:async';
+import 'dart:html';
+
+import 'package:force/force_browser.dart';
+
+class Client {
+  
+  final DivElement log = new DivElement();
+  DivElement statusElement = querySelector('#status');
+ 
+  ForceClient forceClient;
+ 
+  DivElement playListElement = querySelector("#nameslist");
+  
+  //name
+  InputElement nameElement = querySelector("#name");
+  
+  //2 parts
+  DivElement enterScreen = querySelector("#enter_screen");
+  DivElement opponentScreen = querySelector("#opponent_screen");
+  
+  String playName; 
+  
+  Client() {
+    print('start force client!');
+    forceClient = new ForceClient();
+    forceClient.connect();
+    
+    nameElement.onChange.listen((e) {
+      e.preventDefault();
+      
+      playName = nameElement.value;
+      enterScreen.style.display = "none";
+      opponentScreen.style.display = "block";
+      
+      var profileInfo = { 'name' : playName};
+      forceClient.initProfileInfo(profileInfo);
+      
+      forceClient.send('list', {});
+      
+      e.stopPropagation();
+    });
+    
+    nameElement.focus();
+    
+    forceClient.onConnecting.listen((e) {
+      print("connection changed $e");
+      if (e.type=="connected") {
+        onConnected();
+      } else if (e.type=="disconnected") {
+        onDisconnected();
+      }
+    });
+    
+    forceClient.on("list", (e, sender) {
+      addPlayNames(e.json);
+    });
+    
+    forceClient.on("entered", (e, sender) {
+      if (playName!=e.json['name']) {
+        addPlayName(e.json['name']);
+      }
+    });
+    
+    forceClient.on("leaved", (e, sender) {
+      removePlayName(e.json['name']);
+    });
+  }
+
+  void onConnected() {
+    setStatus('');
+    nameElement.disabled = false;
+    
+    forceClient.onMessage.listen((e) {
+      onMessage(e.request, e.json);
+    });
+  }
+
+  void onDisconnected() {
+    setStatus('Disconnected - start \'bin/server.dart\' to continue');
+    nameElement.disabled = true;
+  }
+
+  void setStatus(String status) {
+    statusElement.innerHtml = status;
+  }
+
+
+  void onMessage(String request, dynamic json) {
+     print("response on: '$request' with $json");
+  }
+  
+  void addPlayNames(playnames) { 
+    print("$playnames");
+    playListElement.children.clear();
+    for (var name in playnames) {
+      addPlayName(name);
+    }
+  }
+  
+  void addPlayName(name) {
+    var result = new DivElement();
+    var link = new AnchorElement();
+    link.innerHtml = "$name";
+    result.children.add(link);
+    playListElement.children.add(result);
+    
+    link.onClick.listen((e) {
+      print('opponent of your choice $name');
+    });
+  }
+  
+  void removePlayName(removedName) {
+    print("$removedName will be removed");
+    Element removed;
+    for (Element el in playListElement.children) {
+      if (el.innerHtml == removedName) {
+        removed = el;
+      }
+    }
+    playListElement.children.remove(removed);
+  }
+}
+
+void main() {
+  var client = new Client();
+}
