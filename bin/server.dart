8be5d811a44ee4e7a1fd8a09fb36d10cac34adb9
joaobiriba <glaquidara@gmail.com>
2015-03-05 00:28:27 +0100

    start button

--- bin/server.dart
+++ bin/server.dart
@@ -11,48 +11,69 @@ part 'game.dart';
 final Logger log = new Logger('ChatApp');
 
 void main() {
-  
+
   var portEnv = Platform.environment['PORT'];
   var port = portEnv == null ? 8080 : int.parse(portEnv);
   var serveClient = portEnv == null ? true : false;
-      
-  ForceServer fs = new ForceServer(host: "0.0.0.0", port: port, clientFiles: '../build/web/',
-      clientServe: serveClient, startPage: "game.html");
-  
+
+  const TIMEOUT = const Duration(seconds: 1);
+  var number = 60 * 2;
+
+
+
+  ForceServer fs = new ForceServer(host: "0.0.0.0", port: port, clientFiles: '../build/web/', clientServe: serveClient, startPage: "game.html");
+
   // Setup logger
   fs.setupConsoleLog();
-  
+
+  void handleTimeout() {
+    number = number - 1;
+
+    print("send a number to the clients $number");
+
+    var data = {
+      "count": "$number"
+    };
+    fs.send("updateTime", data);
+  }
+
+
+  startTimeout() {
+    return new Timer(TIMEOUT, handleTimeout);
+  }
+
   // Profile shizzle
   List<String> playerList = new List<String>();
   fs.onProfileChanged.listen((e) {
     String name = e.profileInfo['name'];
     if (e.type == ForceProfileType.New) {
       playerList.add(name);
-      
-      fs.send('entered', { 'name' : name });
+
+      fs.send('entered', {
+        'name': name
+      });
     }
     if (e.type == ForceProfileType.Removed) {
       playerList.remove(name);
-      
-      fs.send('leaved', { 'name' : name });
+
+      fs.send('leaved', {
+        'name': name
+      });
     }
   });
-  
-  fs.on('list', (e, sendable) { 
+
+  fs.on('list', (e, sendable) {
     sendable.sendTo(e.wsId, 'list', playerList);
   });
-  
-  const TIMEOUT = const Duration(seconds: 1);
-  var number = 60*2;
 
-   new Timer.periodic(TIMEOUT, (Timer t) {
-     number = number - 1;
-     
-     print("send a number to the clients $number");
-     
-     var data = { "count" : "$number"};
-     fs.send("updateTime", data);
-     
-   });
+
+
+  fs.on('start', (e, sendable) {
+    print("Start");
+    startTimeout();
+    sendable.sendTo(e.wsId, 'go', playerList);
+
+  });
+
   fs.start();
 }
--- web/client.dart
+++ web/client.dart
@@ -7,122 +7,133 @@ import 'package:stagexl/stagexl.dart';
 import 'block_paint_object.dart';
 
 class Client {
-  
+
   final DivElement log = new DivElement();
   int color = Color.Red;
   Stage stage;
   var uid = "";
   String opponent;
   List<List> playlist;
-  
+
   DivElement statusElement = querySelector('#status');
- 
+
   ForceClient forceClient;
- 
+
   DivElement playListElement = querySelector("#nameslist");
   DivElement emptyList = querySelector("#emptyList");
-  
+
   //name
   InputElement nameElement = querySelector("#name");
-  
+
   //opponent_name
   DivElement opponentElement = querySelector("#opponent_name");
-  
+
   //screens
   DivElement enterScreen = querySelector("#enter_screen");
   DivElement opponentScreen = querySelector("#opponent_screen");
   DivElement gameScreen = querySelector("#game_screen");
-  
-  
-  DivElement timeField = querySelector("#time");
 
-  String playName; 
-  
+
+  DivElement timeField = querySelector("#time");
+  ButtonElement startButton = querySelector("#startButton");
+
+  String playName;
+
   Client() {
     print('start force client!');
     forceClient = new ForceClient();
     forceClient.connect();
-    
+
     nameElement.onChange.listen((e) {
       e.preventDefault();
-      
+
       playName = nameElement.value;
       enterScreen.style.display = "none";
       opponentScreen.style.display = "block";
-      
-      var profileInfo = { 'name' : playName};
+
+      var profileInfo = {
+        'name': playName
+      };
       forceClient.initProfileInfo(profileInfo);
-      
+
       forceClient.send('list', {});
-      
+
       e.stopPropagation();
     });
-    
+
     nameElement.focus();
-    
+
+    startButton.onClick.listen((e) {
+      forceClient.send('start', {});
+    });
+
     forceClient.onConnected.listen((e) {
       onConnected();
     });
-    
+
     forceClient.onDisconnected.listen((e) {
       onDisconnected();
     });
-    
+
     forceClient.on("list", (e, sender) {
       addPlayNames(e.json);
     });
-    
+
+    forceClient.on("go", (e, sender) {
+      goWithTheGame();
+    });
+
     forceClient.on("entered", (e, sender) {
-      if (playName!=e.json['name']) {
+      if (playName != e.json['name']) {
         addPlayName(e.json['name']);
       }
     });
-    
+
     forceClient.on("leave", (e, sender) {
       var names = e.json;
       print('remove names : $names');
-      
+
       for (var name in names) {
         removePlayName(name);
       }
     });
-    
+
     forceClient.on("leaved", (e, sender) {
       removePlayName(e.json['name']);
     });
-    
-    forceClient.on("start_game", (e, sender){
+
+    forceClient.on("start_game", (e, sender) {
       startGame(e.json['opponent']);
       this.uid = e.json['gameId'];
     });
-    
+
     forceClient.on("move", (e, sender) {
       var x = e.json['x'];
       var y = e.json['y'];
       BlockPaint block = playlist[x][y];
-      
+
       block.draw(Color.Blue);
     });
-    
+
 
     forceClient.on("updateTime", (fme, sender) {
       timeField.innerHtml = "${fme.json["count"]}";
       if (int.parse('${fme.json["count"]}') > 10) {
         const TIMEOUT = const Duration(milliseconds: 500);
-        var x=1;
-           new Timer.periodic(TIMEOUT, (Timer t) {
-             var set=1;
-                 if(x==0 && set==1) {
-                   document.body.style.backgroundColor = 'red';             
-                     x=1;
-                     set=0;
-                 }
-                 if(x==1 && set==1) {
-                   document.body.style.backgroundColor = 'white';             
-                     x=0;
-                     set=0;
-                 }
-           });
+        var x = 1;
+        new Timer.periodic(TIMEOUT, (Timer t) {
+          var set = 1;
+          if (x == 0 && set == 1) {
+            document.body.style.backgroundColor = 'red';
+            x = 1;
+            set = 0;
+          }
+          if (x == 1 && set == 1) {
+            document.body.style.backgroundColor = 'white';
+            x = 0;
+            set = 0;
+          }
+        });
       }
     });
   }
@@ -130,7 +141,7 @@ class Client {
   void onConnected() {
     setStatus('');
     nameElement.disabled = false;
-    
+
     forceClient.onMessage.listen((e) {
       onMessage(e.request, e.json);
     });
@@ -147,10 +158,10 @@ class Client {
 
 
   void onMessage(String request, dynamic json) {
-     print("response on: '$request' with $json");
+    print("response on: '$request' with $json");
   }
-  
-  void addPlayNames(playnames) { 
+
+  void addPlayNames(playnames) {
     print("$playnames");
     playListElement.children.clear();
     for (var name in playnames) {
@@ -159,7 +170,7 @@ class Client {
       }
     }
   }
-  
+
   void addPlayName(name) {
     var result = new DivElement();
     var link = new AnchorElement();
@@ -167,69 +178,71 @@ class Client {
     link.innerHtml = "$name";
     result.children.add(link);
     playListElement.children.add(result);
-    
+
     link.onClick.listen((e) {
       print('opponent of your choice $name');
-      
+
       this.uid = forceClient.generateId();
       var request = {
-          'gameId': uid,
-          'opponent': name
+        'gameId': uid,
+        'opponent': name
       };
-      
+
       startGame(name);
       color = Color.Blue;
-      
-      forceClient.send("start", request );
+
+      forceClient.send("start", request);
     });
     emptyList.style.display = "none";
   }
-  
+
   void startGame(String name) {
     opponent = name;
     opponentElement.innerHtml = name;
-    
+
     opponentScreen.style.display = "none";
-    gameScreen.style.display= "block";
-    
+    gameScreen.style.display = "block";
+
     buildPlayField();
   }
-  
+
   void buildPlayField() {
     print("build play field");
     var canvas = querySelector('#stage');
     this.stage = new Stage('myStage', canvas);
     var renderLoop = new RenderLoop();
     renderLoop.addStage(stage);
-    
-    playlist= [[new BlockPaint(color), new BlockPaint(color), new BlockPaint(color)],
-                          [new BlockPaint(color), new BlockPaint(color), new BlockPaint(color)],
-                          [new BlockPaint(color), new BlockPaint(color), new BlockPaint(color)]];
-    
+
+    playlist = [[new BlockPaint(color), new BlockPaint(color), new BlockPaint(color)], [new BlockPaint(color), new BlockPaint(color), new BlockPaint(color)], [new BlockPaint(color), new BlockPaint(color), new BlockPaint(color)]];
+
     /* Painting painting = new Painting();
     stage.addChild(painting); */
-    
-    for (int r = 0; r<3; r++) {
-      for (int c = 0; c<3; c++) {
+
+    for (int r = 0; r < 3; r++) {
+      for (int c = 0; c < 3; c++) {
         BlockPaint block = playlist[r][c];
         print("what is in list for $r on $c");
-       
+
         block.x = r * 100;
         block.y = c * 100;
-        
+
         block.listen().listen((e) {
-            var request = {
-                         'gameId': uid,
-                         'opponent': opponent,
-                         'x': r,
-                         'y': c
-            };
-          
-            forceClient.send("play", request);
+          var request = {
+            'gameId': uid,
+            'opponent': opponent,
+            'x': r,
+            'y': c
+          };
+
+          forceClient.send("play", request);
         });
         stage.addChild(block);
       }
-    } 
+    }
+  }
+  
+  void goWithTheGame() {
+    startButton.hidden = true;
   }
   
   void removePlayName(removedName) {
@@ -247,6 +260,8 @@ class Client {
     }
   }
 }
+
+
 
 void main() {
   var client = new Client();
